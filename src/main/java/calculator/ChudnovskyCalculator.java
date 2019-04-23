87f4402cf43ce6688fac8dd4ca65e82d9faa8deb
Stefan <stscit21@hs-esslingen.de>
2019-04-23 16:39:29 +0200

    Fix calculation error

--- src/main/java/calculator/ChudnovskyCalculator.java
+++ src/main/java/calculator/ChudnovskyCalculator.java
@@ -42,11 +42,10 @@ public class ChudnovskyCalculator implements PiCalculator {
         return result;
     }
 
-    public BigDecimal chudnovsky(int n, MathContext context) {
+    private BigDecimal chudnovsky(int n, MathContext context) {
 
         Supplier<BigDecimal> cons = executeCalculation(() -> chudnovskyConstant(context));
         Supplier<BigDecimal> result = executeCalculation(() -> chudnovskySum(n, context));
-
 
         Supplier<BigDecimal> pi = executeCalculation(() -> cons.get().divide(result.get(), context));
         return pi.get();
@@ -58,42 +57,40 @@ public class ChudnovskyCalculator implements PiCalculator {
         Callable<BigInteger> f2 = () -> factorial(3 * k);
         Callable<BigInteger> f3 = () -> factorial(k);
 
-        Callable<BigDecimal> mul1 = () -> BigDecimal.valueOf(545140134).multiply(BigDecimal.valueOf(k));
-
+        Callable<BigDecimal> mul0 = () -> BigDecimal.valueOf(545140134).multiply(BigDecimal.valueOf(k));
         Callable<BigDecimal> pow1 = () -> BigDecimal.valueOf(-262537412640768000L).pow(k);
 
         Supplier<BigInteger> f1Supplier = executeCalculation(f1);
-        Supplier<BigInteger> f2Supplier = () -> executeCalculation(f2).get();
-        Supplier<BigInteger> f3Supplier = () -> executeCalculation(f3).get();
-        Supplier<BigDecimal> m1Supplier = () -> executeCalculation(mul1).get();
-        Supplier<BigDecimal> pow1Supplier = () -> executeCalculation(pow1).get();
+        Supplier<BigInteger> f2Supplier = executeCalculation(f2);
+        Supplier<BigInteger> f3Supplier = executeCalculation(f3);
+        Supplier<BigDecimal> mul0Supplier = executeCalculation(mul0);
+        Supplier<BigDecimal> pow1Supplier = executeCalculation(pow1);
 
-        Callable<BigDecimal> m2 = () -> new BigDecimal(f1Supplier.get()).multiply(m1Supplier.get());
+        Callable<BigDecimal> addition1 = () -> mul0Supplier.get().add(BigDecimal.valueOf(13591409));
         Callable<BigDecimal> pow2 = () -> new BigDecimal(f3Supplier.get()).pow(3);
 
+        Supplier<BigDecimal> addition1Supplier = executeCalculation(addition1);
+        Callable<BigDecimal> m2 = () -> new BigDecimal(f1Supplier.get()).multiply(addition1Supplier.get());
         Supplier<BigDecimal> m2Supplier = executeCalculation(m2);
         Supplier<BigDecimal> pow2Supplier = executeCalculation(pow2);
 
-        Callable<BigDecimal> addition1 = () -> m2Supplier.get().add(BigDecimal.valueOf(13591409));
         Callable<BigDecimal> m3 = () -> new BigDecimal(f2Supplier.get()).multiply(pow2Supplier.get());
 
-        Supplier<BigDecimal> addition1Supplier = executeCalculation(addition1);
         Supplier<BigDecimal> m3Supplier = executeCalculation(m3);
 
         Callable<BigDecimal> m4 = () -> m3Supplier.get().multiply(pow1Supplier.get());
         Supplier<BigDecimal> m4Supplier = executeCalculation(m4);
-        Supplier<BigDecimal> resultSupplier = () -> addition1Supplier.get().divide(m4Supplier.get(), context).stripTrailingZeros();
+        Supplier<BigDecimal> resultSupplier = () -> m2Supplier.get().divide(m4Supplier.get(), context).stripTrailingZeros();
 
         BigDecimal result = resultSupplier.get();
-        System.out.println("Finished chudnovsky number: " + k);
         return result;
     }
 
     private BigDecimal chudnovskySum(int n, MathContext context) {
         IntStream intStream = IntStream.range(0, n + 1).parallel();
         Stream<Callable<BigDecimal>> lotOfWork = intStream.mapToObj(value -> () -> chudnovskyNumber(value, context));
-        BigDecimal result = lotOfWork.map(this::executeCalculation).map(Supplier::get).reduce(BigDecimal.ZERO, BigDecimal::add);
-        return result;
+        BigDecimal result = lotOfWork.map(this::executeCalculation).map(Supplier::get).reduce(BigDecimal.valueOf(0.0), BigDecimal::add);
+        return result.stripTrailingZeros();
     }
 
     private BigDecimal chudnovskyConstant(MathContext context) {
@@ -104,7 +101,7 @@ public class ChudnovskyCalculator implements PiCalculator {
         Callable<BigDecimal> m1 = () -> BigDecimal.valueOf(426880).multiply(sqrtSupplier.get());
         Supplier<BigDecimal> m1Supplier = executeCalculation(m1);
 
-        return m1Supplier.get();
+        return m1Supplier.get().stripTrailingZeros();
     }
 
     public void setExecutorService(ExecutorService executorService) {
@@ -134,7 +131,22 @@ public class ChudnovskyCalculator implements PiCalculator {
         }
     }
 
-    public void setExecutorService(ExecutorService executorService) {
-        this.executorService = executorService;
+    private void test() {
+        MathContext context = new MathContext(200, RoundingMode.DOWN);
+        BigDecimal chudnovskyConstant = chudnovskyConstant(context);
+        BigDecimal c0 = chudnovskyNumber(0, context);
+        BigDecimal c1 = chudnovskyNumber(1, context);
+        System.out.println("c1 = " + c1);
+        BigDecimal c2 = chudnovskyNumber(2, context);
+        BigDecimal c10 = chudnovskyNumber(10, context);
+
+        BigDecimal pi0 = chudnovskyConstant.divide(c0, context);
+        BigDecimal pi1 = chudnovskyConstant.divide(chudnovskyConstant.divide(pi0, context).add(c1), context);
+        BigDecimal pi2 = chudnovskyConstant.divide(chudnovskyConstant.divide(pi1, context).add(c2), context);
+        BigDecimal pi10 = chudnovskyConstant.divide(chudnovskyConstant.divide(pi2, context).add(c10), context);
+        System.out.println(pi0);
+        System.out.println(pi1);
+        System.out.println(pi2);
+        System.out.println(pi10);
     }
 }
