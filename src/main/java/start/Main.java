9c3a3296da0df7fc37d310bc75b6d2fb8f31478f
Stefan <stscit21@hs-esslingen.de>
2019-10-07 19:59:26 +0200

    Update GUI slightly

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,6 +1,5 @@
 package start;
 
-import calculator.AsyncPiCalculator;
 import calculator.PiCalculator;
 import calculator.impl.ChudnovskyCalculator;
 
@@ -10,6 +9,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,18 +18,19 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     private PiCalculator calculator;
+    ExecutorService service;
 
     public static void main(String[] args) {
         Main main = new Main();
         main.start();
     }
 
+    // TODO needs some clean up. Quick and dirty testing method.
     private void start() {
-        ExecutorService service;
-        //service = Executors.newFixedThreadPool(4); // 1.
-        // service = Executors.newCachedThreadPool(); // 2.
-        service = Executors.newWorkStealingPool(); // 3.
-        AsyncPiCalculator calculator = new ChudnovskyCalculator(service);
+        // service = Executors.newFixedThreadPool(4);
+        // service = Executors.newCachedThreadPool();
+        initExecutorService();
+        calculator = new ChudnovskyCalculator(service);
         //calculator = new BaileyBorweinPlouffeCalculator(service);
 
         JFrame frame = new JFrame();
@@ -41,9 +42,17 @@ public class Main {
         JButton button = new JButton();
         button.setText("Calculate");
 
+        JButton abortButton = new JButton();
+        abortButton.setText("Abort");
+        abortButton.setEnabled(false);
+
         JTextField iterationsField = new JTextField(10);
         iterationsField.setEditable(true);
         iterationsField.setToolTipText("Number of iterations");
+
+        JTextField requiredIterationsField = new JTextField(10);
+        requiredIterationsField.setEditable(false);
+        requiredIterationsField.setToolTipText("Required iterations");
 
         JTextField digitsField = new JTextField(10);
         digitsField.setToolTipText("Number of digits");
@@ -79,26 +88,44 @@ public class Main {
                     int iterations = Integer.parseInt(text);
                     int digits = Integer.parseInt(digitsText);
 
-                    MathContext context = new MathContext(digits);
+                    if (calculator instanceof ChudnovskyCalculator) {
+                        ChudnovskyCalculator chudCalc = (ChudnovskyCalculator) calculator;
+                        int numIterations = chudCalc.getNumIterations(digits);
+                        requiredIterationsField.setText(String.valueOf(numIterations));
+                    } else {
+                        requiredIterationsField.setText("UNKNOWN");
+                    }
+
+                    MathContext context = new MathContext(digits, RoundingMode.HALF_UP);
                     CompletableFuture<BigDecimal> chudnovskyPi = calculator.calculateAsync(iterations, context);
+                    abortButton.setEnabled(true);
                     chudnovskyPi.thenAccept(bigDecimal -> {
                         String decString = bigDecimal.toString();
                         label.setText("Result: " + decString);
                         button.setEnabled(true);
+                        abortButton.setEnabled(false);
                         System.out.println(decString);
                     });
                 }
             };
+            r.run();
+        });
 
-            //Thread t = new Thread(r);
-            //t.start(); // 1.
-            // service.submit(r); // 2.
-            r.run(); // 3.
+        abortButton.addActionListener(e -> {
+            System.out.println("Aborting...");
+            service.shutdownNow();
+            abortButton.setEnabled(false);
+            initExecutorService();
+            calculator.setExecutorService(service);
+            label.setText("Aborted");
+            button.setEnabled(true);
         });
 
         panel.add(button);
         panel.add(iterationsField);
         panel.add(digitsField);
+        panel.add(requiredIterationsField);
+        panel.add(abortButton);
         panel.add(label);
         frame.add(panel);
 
@@ -115,5 +142,9 @@ public class Main {
         });
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void initExecutorService() {
+        service = Executors.newWorkStealingPool();
     }
 }
